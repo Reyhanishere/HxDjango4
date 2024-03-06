358c8849e35804181a554e5a4bc139599c4637ec
Ehsan Kalateh <kalateehsan19@gmail.com>
2024-03-06 19:23:24 +0330

    pages app

--- pages/views.py
+++ pages/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.views.generic import TemplateView
 
-# Create your views here.
+class  MainPageView(TemplateView):
+    template_name = "case_list.html"
+
+class HomePageView(TemplateView):
+    template_name = "home.html"
+
+class AboutPageView(TemplateView):
+    template_name = "about.html"
