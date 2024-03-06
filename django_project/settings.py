511eec6f401c0d114e64490a4a5581595c144b70
Ehsan Kalateh <kalateehsan19@gmail.com>
2024-03-06 20:19:46 +0330

    final configurations

--- django_project/settings.py
+++ django_project/settings.py
@@ -34,6 +34,9 @@ INSTALLED_APPS = [
     "cases.apps.CasesConfig",
 ]
 
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5"
+
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
