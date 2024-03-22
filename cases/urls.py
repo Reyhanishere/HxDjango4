b55292df54aa0fb090406aa007fa09f940726a2a
Ehsan Kalateh <96934557+EhsanKalateh@users.noreply.github.com>
2024-03-22 16:44:18 +0330

    adding Picasso to urls and sepereting hx and case

--- cases/urls.py
+++ cases/urls.py
@@ -1,16 +1,28 @@
 from django.urls import path
+
 from .views import (CasesListView,
                      CaseDetailView, 
                      CaseUpdateView, 
                      CaseDeleteView, 
-                     CaseCreateView)
+                     CaseCreateView,
+                     cases_main,
+                     PicassoCreateView,
+                     PicassoUpdateView,
+                     PicassoListView,
+                     PicassoDetailView,
+                     )
 
 
 urlpatterns = [
-    path("", CasesListView.as_view(), name="cases_list"),
-    path("<slug:slug>/", CaseDetailView.as_view(), name="case_detail"),
-    path("<slug:slug>/edit", CaseUpdateView.as_view(), name="case_edit"),
-    path("<slug:slug>/delete", CaseDeleteView.as_view(), name="case_delete"),
-    path("new", CaseCreateView.as_view(), name="case_new"),
-    
-]
+    path('', cases_main, name='cases_main'),
+    path("picasso/", PicassoListView.as_view(), name="picasso_list"),
+    path("picasso/<slug:slug>/", PicassoDetailView.as_view(), name="picasso_detail"),
+    path("picasso/new", PicassoCreateView.as_view(), name="picasso_new"),
+    path("picasso/<slug:slug>/edit", PicassoUpdateView.as_view(), name="picasso_edit"),
+    path("hx/", CasesListView.as_view(), name="hx_list"),
+    path("hx/<slug:slug>/", CaseDetailView.as_view(), name="hx_detail"),
+    path("hx/<slug:slug>/edit", CaseUpdateView.as_view(), name="hx_edit"),
+    path("hx/<slug:slug>/delete", CaseDeleteView.as_view(), name="hx_delete"),
+    path("hx/new", CaseCreateView.as_view(), name="hx_new"),
+    ]
+
