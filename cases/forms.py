59740857a5197f57d1d09cb5e062fb5f818f6f26
Ehsan Kalateh <96934557+EhsanKalateh@users.noreply.github.com>
2024-03-22 16:42:52 +0330

    add Picasso to Forms

--- cases/forms.py
+++ cases/forms.py
@@ -1,26 +1,39 @@
 from django.forms import ModelForm, CheckboxSelectMultiple, CharField
+from django import forms
 
 
 from .models import (Case, 
                     #  FollowUp, 
-                     Comment)
+                     Comment,
+                     Picasso)
 
 
 class CaseUpdateForm(ModelForm):
-    # cat=CharField(widget=CheckboxSelectMultiple())
     class Meta:
         model = Case
-        exclude = ("slug","verified","author","rating","lang")
+        exclude = ("slug","verified","author","rating","lang","cover","choice")
+
+class CommentForm(ModelForm):
+    class Meta:
+        model = Comment
+        fields = ("comment",)
+        
+class PicassoCreateForm(forms.ModelForm):
+    class Meta:
+        model = Picasso
+        fields = ['title', "image",'description',"text","slug","case"]
+
+class PicassoUpdateForm(ModelForm):
+    class Meta:
+        model = Picasso
+        exclude = ("slug","verified","premium","rating","lang","choice")
 
 # class FollowUpForm(ModelForm):
 #     class Meta:
 #         model = FollowUp
 #         fields = ('date',"text",)
 
-class CommentForm(ModelForm):
-    class Meta:
-        model = Comment
-        fields = ("comment",)
+
 
 # class CaseCreateForm(ModelForm):
 #     class Meta:
