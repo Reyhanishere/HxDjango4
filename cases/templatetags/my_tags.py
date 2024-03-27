ea52672943c6ea144e14e9ccdd81ac56765a414f
Ehsan Kalateh <96934557+EhsanKalateh@users.noreply.github.com>
2024-03-27 12:08:26 +0330

    add markdown

--- cases/templatetags/my_tags.py
+++ cases/templatetags/my_tags.py
@@ -1,7 +1,15 @@
 from django import template
+from django.template.defaultfilters import stringfilter
+
+import markdown as md
 
 register = template.Library()
 
 @register.filter
 def model_type(obj):
     return obj.__class__.__name__
+
+@register.filter()
+@stringfilter
+def markdown(value):
+    return md.markdown(value, extensions=['markdown.extensions.fenced_code'])
