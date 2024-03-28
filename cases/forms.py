767dcbf3741f2daa76eb4ee8a15ba435d3d4b02d
Ehsan Kalateh <96934557+EhsanKalateh@users.noreply.github.com>
2024-03-28 16:51:25 +0330

    add class and id attrs to picasso texts

--- cases/forms.py
+++ cases/forms.py
@@ -2,31 +2,42 @@ from django.forms import ModelForm, CheckboxSelectMultiple, CharField
 from django import forms
 
 
-from .models import (Case, 
-                    #  FollowUp, 
-                     Comment,
-                     Picasso)
+from .models import (
+    Case,
+    #  FollowUp,
+    Comment,
+    Picasso,
+)
 
 
 class CaseUpdateForm(ModelForm):
     class Meta:
         model = Case
-        exclude = ("slug","verified","author","rating","lang","cover","choice")
+        exclude = ("slug", "verified", "author", "rating", "lang", "cover", "choice")
+
 
 class CommentForm(ModelForm):
     class Meta:
         model = Comment
         fields = ("comment",)
-        
+
+
 class PicassoCreateForm(forms.ModelForm):
+    text = forms.CharField(widget=forms.Textarea(attrs={'class':'markdown-editor',
+                                                        'id':'picasso-create-text-editor',}))
+
     class Meta:
         model = Picasso
-        fields = ['title', "image",'description',"text","slug","case"]
+        fields = ["title", "image", "description", "text", "slug", "case"]
+
 
 class PicassoUpdateForm(ModelForm):
+    text = forms.CharField(widget=forms.Textarea(attrs={'class':'markdown-editor',
+                                                        'id':'picasso-edit-text-editor',}))
     class Meta:
         model = Picasso
-        exclude = ("slug","verified","premium","rating","lang","choice","author")
+        exclude = ("slug", "verified", "premium", "rating", "lang", "choice", "author")
+
 
 # class FollowUpForm(ModelForm):
 #     class Meta:
@@ -34,9 +45,7 @@ class PicassoUpdateForm(ModelForm):
 #         fields = ('date',"text",)
 
 
-
 # class CaseCreateForm(ModelForm):
 #     class Meta:
 #         model = Case
 #         exclude = ("author",)
-    
