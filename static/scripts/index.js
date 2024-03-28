d9fe4489c193d364c905107e20c6c7bf2211f4c2
Ehsan Kalateh <96934557+EhsanKalateh@users.noreply.github.com>
2024-03-28 16:52:42 +0330

    add index.js

---
+++ static/scripts/index.js
@@ -0,0 +1,89 @@
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
+
+function addMarkdown(type) {
+    const textarea = document.querySelector('.markdown-editor');
+    const start = textarea.selectionStart;
+    const end = textarea.selectionEnd;
+    const selectedText = textarea.value.substring(start, end);
+    let newText;
+
+    if (selectedText.length !== 0) {
+        switch (type) {
+            case 'bold':
+                newText = `**${selectedText}**`;
+                break;
+            case 'italic':
+                newText = `*${selectedText}*`;
+                break;
+            case 'underline':
+                newText = `<u>${selectedText}</u>`;
+                break;
+            default:
+                return;
+        }
+
+        textarea.value = textarea.value.substring(0, start) + newText + textarea.value.substring(end);
+    }
+    textarea.focus();
+}
+
+function addLink() {
+    const textarea = document.querySelector('.markdown-editor');
+    const start = textarea.selectionStart;
+    const end = textarea.selectionEnd;
+    const selectedText = textarea.value.substring(start, end);
+    let linkTitle = selectedText;
+    if (selectedText.length === 0) {
+        linkTitle = prompt('Enter the title of the link:');
+    }
+    const linkURL = prompt('Enter the URL:');
+    if (linkURL) {
+        const newText = `[${linkTitle}](${linkURL})`;
+        textarea.value = textarea.value.substring(0, start) + newText + textarea.value.substring(end);
+    }
+    textarea.focus();
+}
+
+function addFootnote() {
+    const textarea = document.querySelector('.markdown-editor');
+    const position = textarea.selectionStart;
+    const footnoteNumber = textarea.value.match(/\[\^\d+\]/g);
+    const nextFootnoteNumber = footnoteNumber ? footnoteNumber.length + 1 : 1;
+    const newText = `[^${nextFootnoteNumber}]`;
+    textarea.value = textarea.value.substring(0, position) + newText + textarea.value.substring(position) + `\n\n[^${nextFootnoteNumber}]: `;
+    textarea.focus();
+}
+
+function createMarkdownButtons(divName) {
+    const markdownTextDiv = document.getElementById(divName);
+
+    // Markdown button labels and their respective functions
+    const markdownButtons = [
+        { label: 'Bold', onclick: "addMarkdown('bold')" },
+        { label: 'Italic', onclick: "addMarkdown('italic')" },
+        { label: 'Underline', onclick: "addMarkdown('underline')" },
+        { label: 'Link', onclick: "addLink()" },
+        { label: 'Footnote', onclick: "addFootnote()" }
+    ];
+
+    // Create buttons and append them to the container
+    markdownButtons.forEach(buttonInfo => {
+        const button = document.createElement('button');
+        button.textContent = buttonInfo.label;
+        button.setAttribute('onclick', buttonInfo.onclick);
+        button.setAttribute('class', buttonInfo.class);
+
+        const btnsDiv = document.createElement('div');
+        btnsDiv.setAttribute('class', 'markdown-buttons');
+        btnsDiv.setAttribute('id', 'buttons-div');
+        markdownTextDiv.insertBefore(button, markdownTextDiv.firstChild);
+        const markdownButtonsContainer = document.getElementById("buttons-div");
+        markdownButtonsContainer.appendChild(button);
+    });
+}
