ecf5cac5850767fee0575d6c5ce9b48d1355feb7
Ehsan Kalateh <96934557+EhsanKalateh@users.noreply.github.com>
2024-03-29 20:07:38 +0330

    add CaseImage and LabTestItem

--- cases/admin.py
+++ cases/admin.py
@@ -1,31 +1,50 @@
 from django.contrib import admin
-from .models import Case, Comment, FollowUp, Picasso
+from .models import (
+    Case,
+    Comment,
+    FollowUp,
+    Picasso,
+    Choice,
+    Tag,
+    LabTestItem,
+    CaseImage,
+)
 
 
-class FollowUpInline(admin.TabularInline):  # new
+class FollowUpInline(admin.TabularInline):
     model = FollowUp
     extra = 0
+
 
 class CommentInline(admin.TabularInline):  # new
     model = Comment
     extra = 0
 
+
+class LabInline(admin.TabularInline):
+    model = LabTestItem
+    extra = 1
+
+
+class CaseImageInline(admin.TabularInline):
+    model = CaseImage
+    extra = 1
+
+
 class CaseAdmin(admin.ModelAdmin):  # new
-    inlines = [
-        FollowUpInline,
-        CommentInline,
-    ]
-    list_display = [
-    "title", "author","date_created","verified"
-    ]
+    inlines = [FollowUpInline, CommentInline, LabInline, CaseImageInline]
+    list_display = ["title", "author", "date_created", "verified"]
+
 
 class PicassoAdmin(admin.ModelAdmin):
-    list_display = [
-    "title", "author","verified","date_created"
-    ]
+    list_display = ["title", "author", "verified", "date_created"]
 
+
+admin.site.register(Choice)
+admin.site.register(Tag)
 admin.site.register(Case, CaseAdmin)
-admin.site.register(Picasso,PicassoAdmin)
-admin.site.register(FollowUp)
+admin.site.register(Picasso, PicassoAdmin)
+# admin.site.register(FollowUp)
 admin.site.register(Comment)
-
+# admin.site.register(LabTestItem)
+admin.site.register(CaseImage)
