39da5fb1664f0a5bb7bea9b71e695709790c41b8
Ehsan Kalateh <96934557+EhsanKalateh@users.noreply.github.com>
2024-03-30 11:17:25 +0330

    Update urls

--- cases/urls.py
+++ cases/urls.py
@@ -14,8 +14,8 @@ from .views import (CasesListView,
                      LabTestCreateView,
                      SuccessPageView,
                      CaseImageView,
+            
                      )
-
 
 urlpatterns = [
     path('', cases_main, name='cases_main'),
@@ -29,8 +29,7 @@ urlpatterns = [
     path("hx/<slug:slug>/delete", CaseDeleteView.as_view(), name="hx_delete"),
     path("hx/new", CaseCreateView.as_view(), name="hx_new"),
     path("search/", SearchResultsListView.as_view(), name="search_results"),
+    path('hx/new/lab', LabTestCreateView.as_view(), name='lab-create'),
     path("success/", SuccessPageView.as_view(), name="success"),
     path('hx/<slug:case_slug>/add-image', CaseImageView.as_view(), name='hx_add_image'),
-
     ]
-
