21b8b68e12e25928669c42403670328a26b4ef68
Ehsan Kalateh <96934557+EhsanKalateh@users.noreply.github.com>
2024-03-30 15:13:38 +0330

    Update forms.py

--- cases/forms.py
+++ cases/forms.py
@@ -1,4 +1,4 @@
-from django.forms import ModelForm
+from django.forms import ModelForm, CheckboxSelectMultiple, CharField
 from django import forms
 
 
@@ -7,19 +7,25 @@ from .models import (
     #  FollowUp,
     Comment,
     Picasso,
-    #ImageCase,
+    # LabTestItem,
+    ImageCase,
 )
 
-# class CaseImageForm(ModelForm):
-#     class Meta:
-#         model = ImageCase
-#         exclude = ("case", "verified", "visible")
+# LabTestForm = forms.inlineformset_factory(
+#     Case, LabTestItem, fields="__all__", extra=1, can_delete=True, can_order=False
+# )
+
+
+class CaseImageForm(ModelForm):
+    class Meta:
+        model = ImageCase
+        exclude = ("case", "verified", "visible")
 
 
 class CaseUpdateForm(ModelForm):
     class Meta:
         model = Case
-        exclude = ("slug", "verified", "author", "rating", "lang", "cover", "choice")
+        exclude = ("slug", "verified", "author", "rating", "lang", "cover", "choice","done","visible")
 
 
 class CommentForm(ModelForm):
@@ -29,16 +35,42 @@ class CommentForm(ModelForm):
 
 
 class PicassoCreateForm(forms.ModelForm):
-    text = forms.CharField(widget=forms.Textarea(attrs={'class':'markdown_editor',
-                                                        'id':'picasso_text_editor',}))
+    text = forms.CharField(
+        widget=forms.Textarea(
+            attrs={
+                "class": "markdown_editor",
+                "id": "picasso_text_editor",
+            }
+        )
+    )
+
     class Meta:
         model = Picasso
-        fields = ["title", "image", "description", "text", "slug"]
+        fields = ["title", "image", "description", "text", "slug", "case"]
 
 
 class PicassoUpdateForm(ModelForm):
-    text = forms.CharField(widget=forms.Textarea(attrs={'class':'markdown_editor',
-                                                        'id':'picasso_text_editor',}))
+    text = forms.CharField(
+        widget=forms.Textarea(
+            attrs={
+                "class": "markdown_editor",
+                "id": "picasso_text_editor",
+            }
+        )
+    )
+
     class Meta:
         model = Picasso
         exclude = ("slug", "verified", "premium", "rating", "lang", "choice", "author")
+
+
+# class FollowUpForm(ModelForm):
+#     class Meta:
+#         model = FollowUp
+#         fields = ('date',"text",)
+
+
+# class CaseCreateForm(ModelForm):
+#     class Meta:
+#         model = Case
+#         exclude = ("author",)
