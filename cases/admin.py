f12b027eeda7fe4054522dfe8058f526e720d068
Ehsan Kalateh <96934557+EhsanKalateh@users.noreply.github.com>
2024-03-30 11:20:12 +0330

    only add imagecase

--- cases/admin.py
+++ cases/admin.py
@@ -1,50 +1,31 @@
 from django.contrib import admin
-from .models import (
-    Case,
-    Comment,
-    FollowUp,
-    Picasso,
-    # Choice,
-    # Tag,
-    # LabTestItem,
-    ImageCase,
-)
+from .models import Case, Comment, FollowUp, Picasso
 
 
-class FollowUpInline(admin.TabularInline):
-    model = FollowUp
-    extra = 0
-
+# class FollowUpInline(admin.TabularInline): 
+#     model = FollowUp
+#     extra = 0
 
 class CommentInline(admin.TabularInline):  # new
     model = Comment
     extra = 0
 
-
-# class LabInline(admin.TabularInline):
-#     model = LabTestItem
-#     extra = 1
-
-
-class CaseImageInline(admin.TabularInline):
-    model = ImageCase
-    extra = 1
-
-
-class CaseAdmin(admin.ModelAdmin):
-    inlines = [FollowUpInline, CommentInline, CaseImageInline] #LabInline,
-    list_display = ["title", "author", "date_created", "verified"]
-
+class CaseAdmin(admin.ModelAdmin):  # new
+    inlines = [
+        # FollowUpInline,
+        CommentInline,
+    ]
+    list_display = [
+    "title", "author","date_created","verified"
+    ]
 
 class PicassoAdmin(admin.ModelAdmin):
-    list_display = ["title", "author", "verified", "date_created"]
+    list_display = [
+    "title", "author","verified","date_created"
+    ]
 
-
-# admin.site.register(Choice)
-# admin.site.register(Tag)
 admin.site.register(Case, CaseAdmin)
-admin.site.register(Picasso, PicassoAdmin)
+admin.site.register(Picasso,PicassoAdmin)
 # admin.site.register(FollowUp)
 admin.site.register(Comment)
-# admin.site.register(LabTestItem)
-admin.site.register(ImageCase)
+
