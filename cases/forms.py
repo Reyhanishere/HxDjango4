71b6ec734b40b73ddf31b82d6503d938dd587650
Ehsan Kalateh <96934557+EhsanKalateh@users.noreply.github.com>
2024-03-30 11:19:50 +0330

    Only add ImageCase

--- cases/forms.py
+++ cases/forms.py
@@ -1,4 +1,4 @@
-from django.forms import ModelForm, CheckboxSelectMultiple, CharField
+from django.forms import ModelForm
 from django import forms
 
 
@@ -7,14 +7,8 @@ from .models import (
     #  FollowUp,
     Comment,
     Picasso,
-    LabTestItem,
     ImageCase,
 )
-
-LabTestForm = forms.inlineformset_factory(
-    Case, LabTestItem, fields="__all__", extra=1, can_delete=True, can_order=False
-)
-
 
 class CaseImageForm(ModelForm):
     class Meta:
@@ -35,14 +29,8 @@ class CommentForm(ModelForm):
 
 
 class PicassoCreateForm(forms.ModelForm):
-    text = forms.CharField(
-        widget=forms.Textarea(
-            attrs={
-                "class": "markdown_editor",
-                "id": "picasso_text_editor",
-            }
-        )
-    )
+    text = forms.CharField(widget=forms.Textarea(attrs={'class':'markdown_editor',
+                                                        'id':'picasso_text_editor',}))
 
     class Meta:
         model = Picasso
@@ -50,27 +38,8 @@ class PicassoCreateForm(forms.ModelForm):
 
 
 class PicassoUpdateForm(ModelForm):
-    text = forms.CharField(
-        widget=forms.Textarea(
-            attrs={
-                "class": "markdown_editor",
-                "id": "picasso_text_editor",
-            }
-        )
-    )
-
+    text = forms.CharField(widget=forms.Textarea(attrs={'class':'markdown_editor',
+                                                        'id':'picasso_text_editor',}))
     class Meta:
         model = Picasso
         exclude = ("slug", "verified", "premium", "rating", "lang", "choice", "author")
-
-
-# class FollowUpForm(ModelForm):
-#     class Meta:
-#         model = FollowUp
-#         fields = ('date',"text",)
-
-
-# class CaseCreateForm(ModelForm):
-#     class Meta:
-#         model = Case
-#         exclude = ("author",)
