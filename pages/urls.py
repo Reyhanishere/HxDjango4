cd9e3fb7821474599d90013a70ae92df14d9ea63
Ehsan Kalateh <96934557+EhsanKalateh@users.noreply.github.com>
2024-04-05 17:08:43 +0330

    add terms

--- pages/urls.py
+++ pages/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import HomePageView, MainPageView, AboutPageView
+from .views import HomePageView, AboutPageView, TermsPageView
 
 urlpatterns = [
     path("", HomePageView.as_view(), name="home"),
     path("about/", AboutPageView.as_view(), name="about"),
-    
+    path("terms/", TermsPageView.as_view(), name="terms"),
 ]
