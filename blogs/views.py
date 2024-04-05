37258edbfbb688dfc73367624362599946f7fc60
Ehsan Kalateh <96934557+EhsanKalateh@users.noreply.github.com>
2024-04-05 17:03:22 +0330

    Update template path

--- blogs/views.py
+++ blogs/views.py
@@ -1,7 +1,6 @@
-from django.shortcuts import render
 from django.views.generic import ListView
 from .models import Post
 
 class PostsList(ListView):
     model = Post
-    template_name = "posts_list.html"
+    template_name = "pages/posts_list.html"
