12d5e1348f33a6e5e33737736318ef07c106c804
Ehsan Kalateh <96934557+EhsanKalateh@users.noreply.github.com>
2024-04-05 17:02:13 +0330

    add more fields into admin panel

--- accounts/admin.py
+++ accounts/admin.py
@@ -3,24 +3,60 @@ from django.contrib.auth.admin import UserAdmin
 from .forms import CustomUserCreationForm, CustomUserChangeForm
 from .models import CustomUser
 
+
 class CustomUserAdmin(UserAdmin):
     add_form = CustomUserCreationForm
     form = CustomUserChangeForm
     model = CustomUser
     list_display = [
-    "username",
-    "first_name",
-    "last_name",
-    "field",
-    "degree",
-    "university",
-    "is_article_author",
-    "is_article_editor",
-    "is_case_editor",
-    "is_staff",
+        "username",
+        "first_name",
+        "last_name",
+        "field",
+        "degree",
+        "university",
+        "is_article_author",
+        "is_article_editor",
+        "is_case_editor",
+        "is_staff",
     ]
-    fieldsets = UserAdmin.fieldsets + ((None, {"fields": ("university","degree")}),)
-    add_fieldsets = UserAdmin.add_fieldsets + ((None, {"fields": ("university","degree")}),)
-    list_display_links = ('username',)
+    fieldsets = UserAdmin.fieldsets + (
+        (
+            None,
+            {
+                "fields": (
+                    "field",
+                    "university",
+                    "degree",
+                    "fn_fa",
+                    "ln_fa",
+                    "is_article_author",
+                    "is_article_editor",
+                    "is_case_editor",
+                    "about_me",
+                )
+            },
+        ),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (
+            None,
+            {
+                "fields": (
+                    "field",
+                    "university",
+                    "degree",
+                    "fn_fa",
+                    "ln_fa",
+                    "is_article_author",
+                    "is_article_editor",
+                    "is_case_editor",
+                    "about_me",
+                )
+            },
+        ),
+    )
+    list_display_links = ("username",)
+
 
 admin.site.register(CustomUser, CustomUserAdmin)
