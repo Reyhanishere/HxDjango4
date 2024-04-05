b4ad58b07864bd28648c235ba7d5a4727f008215
Ehsan Kalateh <96934557+EhsanKalateh@users.noreply.github.com>
2024-04-05 17:01:31 +0330

    some fields changed

--- accounts/models.py
+++ accounts/models.py
@@ -1,15 +1,15 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+
 class CustomUser(AbstractUser):
-    FIELDS=[        
-        ("MD/Student", "MD"),
-        ("Dentist/Student", "Dent"),
-        ("Nurse/Student", "Nurse"),
-        ("Other", "Other"),
-        ("Not Medical Science", "NMS"),
-        
-]
+    FIELDS = [
+        ("MD", "MD/Student" ),
+        ("Dent", "Dentist/Student" ),
+        ("Nurse", "Nurse/Student" ),
+        ("Other", "Other" ),
+        ("NMS", "Not Medical Science" ),
+    ]
     DEGREES = [
         ("Basic Sciences", "Basic Sciences"),
         ("Physiopathology", "Physiopathology"),
@@ -19,64 +19,86 @@ class CustomUser(AbstractUser):
         ("Medical Resident", "Medical Resident"),
         ("Medical Specialist", "Medical Specialist"),
         ("Fellowship", "Fellowship"),
-        ("Superspecialist","Superspecialist"),
+        ("Superspecialist", "Superspecialist"),
         # " ", "-None-",
     ]
     UNIS = [
-        ("علوم پزشکی مشهد","MUMS"),
-        ( "علوم پزشکی تهران","TUMS"),
-        ("علوم پزشکی خراسان شمالی","NKUMS"),
-        ("علوم پزشکی بهشتی","SBMU",),
-        ("علوم پزشکی شیراز","SUMS"),
-        ("علوم پزشکی ایران","IUMS"),
-        ("علوم پزشکی اصفهان","MUI"),
-        ("علوم پزشکی گرگان","GOUMS"),
-        ("علوم پزشکی گیلان","GUMS"),
-        ("علوم پزشکی تبریز","TBZMed"),
-        ("علوم پزشکی ساری","MazUMS"),
-        ("علوم پزشکی بابل","MUBabol"),
-        ("علوم پزشکی جندی‌شاپور اهواز","AJUMS"),
-        ("علوم پزشکی بقیة‌الله","BMSU"),
-        ("علوم پزشکی کرمان","KMU"),
-        ("علوم پزشکی ارومیه","UMSU"),
-        ("علوم پزشکی همدان","UMSHa"),
-        ("علوم پزشکی سبزوار","MedSab"),
-        ("علوم پزشکی بوشهر","BPUMS"),
-        ("علوم پزشکی سمنان","SemUMS"),
-        ("علوم پزشکی هرمزگان","HUMS"),
-        ("علوم پزشکی کرمانشاه","KUMS"),
-        ("علوم پزشکی البرز","ABZUMS"),
-        ("علوم پزشکی بیرجند","ABZUMS"),
-        ("علوم پزشکی زابل","ABZUMS"),
-        ("علوم پزشکی زاهدان","ABZUMS"),
-        ("علوم پزشکی یزد","SSU"),
-        ("علوم پزشکی شهرکرد","SKUMS"),
-        ("علوم پزشکی شاهرود","ShMU"),
-        ("علوم پزشکی نیشابور","NUMS"),
-        ("علوم پزشکی یاسوج","YUMS"),
-        ("علوم پزشکی لرستان","LUMS"),
-        ("علوم پزشکی کردستان","MUK"),
-        ("علوم پزشکی قم","MUQ"),
-        ("علوم پزشکی قزوین","QUMS"),
-        ("Other", "----"),
+        ("MUMS", "علوم پزشکی مشهد" ),
+        ("TUMS", "علوم پزشکی تهران" ),
+        ("NKUMS", "علوم پزشکی خراسان شمالی" ),
+        (
+            "SBMU", "علوم پزشکی بهشتی",
+        ),
+        ("SUMS", "علوم پزشکی شیراز" ),
+        ("IUMS", "علوم پزشکی ایران" ),
+        ("MedIAU", "دانشگاه آزاد"),
+        ("MUI", "علوم پزشکی اصفهان" ),
+        ("GOUMS", "علوم پزشکی گرگان" ),
+        ("GUMS", "علوم پزشکی گیلان" ),
+        ("TBZMed", "علوم پزشکی تبریز" ),
+        ("MazUMS", "علوم پزشکی ساری" ),
+        ("MUBabol", "علوم پزشکی بابل" ),
+        ("AJUMS", "علوم پزشکی جندی‌شاپور اهواز" ),
+        ("BMSU", "علوم پزشکی بقیة‌الله" ),
+        ("KMU", "علوم پزشکی کرمان" ),
+        ("UMSU", "علوم پزشکی ارومیه" ),
+        ("UMSHa", "علوم پزشکی همدان" ),
+        ("MedSab", "علوم پزشکی سبزوار" ),
+        ("BPUMS", "علوم پزشکی بوشهر" ),
+        ("SemUMS", "علوم پزشکی سمنان" ),
+        ("HUMS", "علوم پزشکی هرمزگان" ),
+        ("KUMS", "علوم پزشکی کرمانشاه" ),
+        ("ABZUMS", "علوم پزشکی البرز" ),
+        ("BUMS", "علوم پزشکی بیرجند" ),
+        ("ZUMS", "علوم پزشکی زابل" ),
+        ("ZAUMS", "علوم پزشکی زاهدان" ),
+        ("SSU", "علوم پزشکی یزد" ),
+        ("SKUMS", "علوم پزشکی شهرکرد" ),
+        ("ShMU", "علوم پزشکی شاهرود" ),
+        ("NUMS", "علوم پزشکی نیشابور" ),
+        ("YUMS", "علوم پزشکی یاسوج" ),
+        ("LUMS", "علوم پزشکی لرستان" ),
+        ("MUK", "علوم پزشکی کردستان" ),
+        ("MUQ", "علوم پزشکی قم" ),
+        ("QUMS", "علوم پزشکی قزوین" ),
+        ("----", "Other" ),
     ]
     # , help_text="بهتر است به فارسی بنویسید."
-    first_name= models.CharField(max_length=50,)
-    last_name= models.CharField(max_length=50)
-    field=models.CharField(max_length=32, choices=FIELDS, null=False, blank=True,)
-    degree = models.CharField(max_length=20, choices=DEGREES, null=True, blank=True,
-            help_text=(
-            "Your current degree. Leave empty if not Medical Doctor/Student."
-        ),)
+    first_name = models.CharField(
+        max_length=50,
+    )
+    last_name = models.CharField(max_length=50)
+    field = models.CharField(
+        max_length=32,
+        choices=FIELDS,
+        null=False,
+        blank=True,
+    )
+    degree = models.CharField(
+        max_length=20,
+        choices=DEGREES,
+        null=True,
+        blank=True,
+        help_text=("Your current degree. Leave empty if not Medical Doctor/Student."),
+    )
 
-    university= models.CharField(max_length=30, choices=UNIS, null=True, blank=True,
-            help_text=(
-            "Your current university. Leave empty if not a student."
-        ),)
-    is_article_author=models.BooleanField(blank=True, null=True)
-    is_article_editor=models.BooleanField(blank=True, null=True)
-    is_case_editor=models.BooleanField(blank=True, null=True)
+    university = models.CharField(
+        max_length=30,
+        choices=UNIS,
+        null=True,
+        blank=True,
+        help_text=("Your current university. Leave empty if not a student."),
+    )
+    is_article_author = models.BooleanField(default=False, blank=False, null=False)
+    is_article_editor = models.BooleanField(default=False, blank=False, null=False)
+    is_case_editor = models.BooleanField(default=False, blank=False, null=False)
 
-    fn_fa= models.CharField("نام به فارسی",max_length=50,blank=True, null=True)
-    ln_fa= models.CharField("نام خانوادگی به فارسی",max_length=50,blank=True, null=True)
-    about_me=models.TextField("دربارۀ من",blank=True, null=True)
+    fn_fa = models.CharField("نام به فارسی", max_length=50, blank=True, null=True)
+    ln_fa = models.CharField(
+        "نام خانوادگی به فارسی", max_length=50, blank=True, null=True
+    )
+    about_me = models.TextField(
+        "دربارۀ من",
+        blank=True,
+        null=True,
+    )
