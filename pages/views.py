6e92ea6a36132198d2dc7e0f0eb2603d2fadca9c
Ehsan Kalateh <96934557+EhsanKalateh@users.noreply.github.com>
2024-06-09 21:03:47 +0330

    add graphs

--- pages/views.py
+++ pages/views.py
@@ -3,8 +3,13 @@ from django.views.generic import TemplateView
 class HomePageView(TemplateView):
     template_name = "pages/index.html"
 
+
 class AboutPageView(TemplateView):
     template_name = "pages/about.html"
 
+
 class TermsPageView(TemplateView):
     template_name = "pages/terms.html"
+
+class GraphsPageView(TemplateView):
+    template_name = "pages/graph.html"
