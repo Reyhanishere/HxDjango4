a85070ec76d104065d2807adba40c368a5658992
Ehsan Kalateh <96934557+EhsanKalateh@users.noreply.github.com>
2024-06-09 21:04:13 +0330

    add graphs

--- pages/urls.py
+++ pages/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import HomePageView, AboutPageView, TermsPageView
+from .views import HomePageView, AboutPageView, TermsPageView, GraphsPageView
 
 urlpatterns = [
     path("", HomePageView.as_view(), name="home"),
     path("about/", AboutPageView.as_view(), name="about"),
     path("terms/", TermsPageView.as_view(), name="terms"),
+    path("cases/graphs/", GraphsPageView.as_view(), name="graphs"),
+   
 ]
