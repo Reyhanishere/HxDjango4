3dfe97ac2b0774fd54b72177bbb092ee40222f0b
Ehsan Kalateh <96934557+EhsanKalateh@users.noreply.github.com>
2024-06-10 17:46:59 +0330

    add InlineGraph and Graph itself

--- cases/admin.py
+++ cases/admin.py
@@ -1,18 +1,5 @@
 from django.contrib import admin
-from .models import (
-    Case,
-    Comment,
-    FollowUp,
-    Picasso,
-    Note,
-    Choice,
-    Tag,
-    # LabTestItem,
-    ImageCase,
-    Suggest,
-    Rotation,
-    Reply
-)
+from .models import *
 
 
 class FollowUpInline(admin.TabularInline):
@@ -28,15 +15,12 @@ class ReplyInline(admin.TabularInline):
     model=Reply
     extra=0
 
-# class LabInline(admin.TabularInline):
-#     model = LabTestItem
-#     extra = 1
-
-
 class CaseImageInline(admin.TabularInline):
     model = ImageCase
     extra = 1
-
+class LabGraphInline(admin.TabularInline):
+    model=LabGraphSelection
+    extra=1
 @admin.action(description="Deverify selected cases")
 def deverify_case(Case, request, queryset):
     queryset.update(verified=False)
@@ -46,7 +30,7 @@ def verify_case(Case, request, queryset):
     queryset.update(verified=True)
 
 class CaseAdmin(admin.ModelAdmin):  # new
-    inlines = [FollowUpInline, CommentInline, CaseImageInline]
+    inlines = [FollowUpInline, CommentInline, CaseImageInline, LabGraphInline]
     list_display = ["title", "author", "date_created", "verified","visible"]
     actions = [verify_case,deverify_case]
 
@@ -82,7 +66,6 @@ admin.site.register(Note,NotesAdmin)
 # admin.site.register(FollowUp)
 admin.site.register(Comment, CommentsAdmin)
 admin.site.register(Reply)
-
-# admin.site.register(LabTestItem)
+admin.site.register(LabGraphSelection)
 
 
