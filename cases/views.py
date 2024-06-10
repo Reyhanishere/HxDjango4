f706e9316380a22c633094a31a8e8767d86f5b47
Ehsan Kalateh <96934557+EhsanKalateh@users.noreply.github.com>
2024-06-10 17:45:16 +0330

    Add Graph List Create

--- cases/views.py
+++ cases/views.py
@@ -12,20 +12,8 @@ from django.db.models import Q
 from django.urls import reverse_lazy, reverse
 from django.shortcuts import render, redirect, get_object_or_404
 
-from .models import Case, Picasso, FollowUp, Comment, ImageCase, Note, Reply
-from .forms import (
-    CaseCreateForm,
-    CaseUpdateForm,
-    # FollowUpForm,
-    CommentForm,
-    PicassoCreateForm,
-    PicassoUpdateForm,
-    # LabTestForm,
-    CaseImageForm,
-    ExCreateForm,
-    ExUpdateForm,
-    ReplyForm,
-)
+from .models import *
+from .forms import *
 
 
 class CasesListView(ListView):
@@ -65,18 +53,6 @@ class CommentPost(SingleObjectMixin, FormView):  # new
 
 
 class CaseDetailView(View):
-    # def get(self, request, *args, **kwargs):
-    #     view = CommentGet.as_view()
-    #     return view(request, *args, **kwargs)
-
-    # def post(self, request, *args, **kwargs):
-    #     view = CommentPost.as_view()
-    #     return view(request, *args, **kwargs)
-
-    # def get_context_data(self, **kwargs):  # new
-    #     context = super().get_context_data(**kwargs)
-    #     context["form"] = CommentForm()
-    #     return context
     def get(self, request, *args, **kwargs):
         case = Case.objects.get(slug=kwargs.get('slug'))
         comments = Comment.objects.filter(case=case)
@@ -103,7 +79,7 @@ class CaseDetailView(View):
             return redirect(reverse('hx_detail', kwargs={'slug': case_slug}))
         else:
             return redirect(reverse('hx_detail', kwargs={'slug': case_slug}))
-
+        
 def add_reply(request):
     if request.method == 'POST':
         comment_id = request.POST.get('comment_id')
@@ -166,41 +142,6 @@ class CaseCreateView(LoginRequiredMixin, CreateView):  # new
     model = Case
     template_name = "hx_new.html"
     form_class = CaseCreateForm
-    # formset = LabTestForm
-    # fields = (
-    #     "title",
-    #     "rts",
-    #     "description",
-    #     "pretext",
-    #     "gender",
-    #     "location",
-    #     "job",
-    #     "dwelling",
-    #     "age",
-    #     "marriage",
-    #     "doctor",
-    #     "source",
-    #     "reliability",
-    #     "setting",
-    #     "cc",
-    #     "pi",
-    #     "pmh",
-    #     "drg",
-    #     "sh",
-    #     "fh",
-    #     "alg",
-    #     "ros",
-    #     "phe",
-    #     "dat",
-    #     "summary",
-    #     "ddx",
-    #     "pdx",
-    #     "act",
-    #     "post_text",
-    #     "tags",
-    #     "slug",
-    #     "suggests",
-    # )
 
     success_url = "/cases/success/"
 
@@ -211,15 +152,6 @@ class CaseCreateView(LoginRequiredMixin, CreateView):  # new
     def form_valid(self, form):
         form.instance.author = self.request.user
         return super().form_valid(form)
-
-    # def get_context_data(self, **kwargs):
-    #     data = super().get_context_data(**kwargs)
-    #     if self.request.POST:
-    #         data["formset"] = LabTestForm(self.request.POST)
-    #     else:
-    #         data["formset"] = LabTestForm()
-    #     print(data["formset"])  # Add this line to check formset contents
-    #     return data
 
 
 class CaseImageView(LoginRequiredMixin, UserPassesTestMixin, CreateView):
@@ -241,7 +173,7 @@ class CaseImageView(LoginRequiredMixin, UserPassesTestMixin, CreateView):
 class CasePresentationView(DetailView):
     model=Case
     template_name="hx_presentation.html"
-    
+
 class PicassoListView(ListView):
     model = Picasso
     template_name = "picasso_list.html"
@@ -382,9 +314,35 @@ class ExDeleteView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
         obj = self.get_object()
         return obj.author == self.request.user
 
+class FreeGraphCreateView(LoginRequiredMixin, CreateView):
+    model=LabGraphSelection
+    form_class=FreeGraphForm
+    template_name=""
+    success_url = "/cases/graphs/"
+    
+class CaseGraphCreateView(LoginRequiredMixin, UserPassesTestMixin, CreateView):
+    model=LabGraphSelection
+    form_class=FreeGraphForm # the same as above
+    template_name="hx/graph/graph_new.html"
 
-# class LabTestCreateView(CreateView):
-#     model = LabTestItem
-#     form_class = LabTestForm
-#     template_name = "labtest_create.html"
-#     success_url = "cases/success"  # Update with your success URL
+    def get_success_url(self):
+        case_slug = self.kwargs["case_slug"]
+        case = get_object_or_404(Case, slug=case_slug)
+        return f"/cases/hx/{case.slug}/graphs"
+
+    def form_valid(self, form):
+        case_slug = self.kwargs["case_slug"]
+        case = get_object_or_404(Case, slug=case_slug, author=self.request.user)
+        form.instance.case = case
+        form.instance.author=self.request.user
+        return super().form_valid(form)
+    def test_func(self):
+        case_slug = self.kwargs["case_slug"]
+        case = get_object_or_404(Case, slug=case_slug, author=self.request.user)
+        return case.author == self.request.user
+
+class GraphListView(ListView):
+    model=LabGraphSelection
+    context_object_name = "graphs"
+    template_name="hx/graph/graph_list.html"
+
