069e4750321db2c7b40290bc44482bbe8c156334
Ehsan Kalateh <96934557+EhsanKalateh@users.noreply.github.com>
2024-08-14 16:58:27 +0330

    add user_panel function view

--- accounts/views.py
+++ accounts/views.py
@@ -2,9 +2,14 @@ from django.urls import reverse_lazy
 from django.views.generic import CreateView
 
 from .forms import CustomUserCreationForm
-
+import cases.models as Cases
 
 class SignUpView(CreateView):
     form_class = CustomUserCreationForm
     success_url = reverse_lazy('login')
     template_name = "registration/signup.html"
+
+def user_panel(request):
+    user = request.user
+    hxs = Cases.Case.objects.filter(author=user)
+    return render(request, 'user/user_panel.html', {'hxs': hxs,})
