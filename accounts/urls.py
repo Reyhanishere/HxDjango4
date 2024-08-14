8ef0f27b2ffdba9c19d6080b0221c38dbda7a9d8
Ehsan Kalateh <96934557+EhsanKalateh@users.noreply.github.com>
2024-08-14 17:01:30 +0330

    add self_user_cases

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import SignUpView
+from .views import SignUpView, self_user_cases
 
 urlpatterns = [
     path("signup/", SignUpView.as_view(), name="signup"),
+    path("dashboard/", self_user_cases, name="self_user_cases"),
 ]
