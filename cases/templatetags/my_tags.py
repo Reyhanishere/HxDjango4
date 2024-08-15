fbb8c5c7e88b6e1dbb07b88d87a55998d3dbe67d
Ehsan Kalateh <96934557+EhsanKalateh@users.noreply.github.com>
2024-08-15 19:57:12 +0330

    add markdown for ex

--- cases/templatetags/my_tags.py
+++ cases/templatetags/my_tags.py
@@ -1,6 +1,22 @@
 from django import template
+from django.template.defaultfilters import stringfilter
+
+import markdown as md
 
 register = template.Library()
+
+
+@register.filter
+def model_type(obj):
+    return obj.__class__.__name__
+
+@register.filter()
+@stringfilter
+def markdown(value):
+    return md.markdown(value, extensions=['markdown.extensions.fenced_code',
+                                          'footnotes',
+                                          'tables',
+                                          'toc'])
 
 @register.filter(name='emphasize')
 def emphasize(value):
