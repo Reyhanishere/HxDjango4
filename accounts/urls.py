7ddc2d71e8fbe2b2a676d9529f1b036877b9962b
Ehsan Kalateh <96934557+EhsanKalateh@users.noreply.github.com>
2024-08-15 19:06:06 +0330

    delete login required

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path
 from .views import SignUpView, self_user_cases
-from django.contrib.auth.decorators import login_required
 
 urlpatterns = [
     path("signup/", SignUpView.as_view(), name="signup"),
