6b2b86243a23d4b79474593324f9761df7151baf
Ehsan Kalateh <96934557+EhsanKalateh@users.noreply.github.com>
2024-09-17 17:35:31 +0330

    Add UserChangeInfoView and import some

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,9 @@
 from django.urls import reverse_lazy
-from django.views.generic import CreateView
-from django.shortcuts import render
+from django.views.generic import CreateView, UpdateView
+from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+
 
 from .forms import CustomUserCreationForm
 import cases.models as Cases
@@ -17,3 +19,12 @@ def self_user_cases(request):
     hxs = Cases.Case.objects.filter(author=user).order_by("-date_created")
     return render(request, 'user/self_user_cases.html', {'hxs': hxs,})
 
+class UserChangeInfoView(UserPassesTestMixin, LoginRequiredMixin, UpdateView):
+    model= CustomUser
+    template_name='registration/change_info.html'
+    fields=['first_name', 'last_name', 'degree', 'university','fn_fa','ln_fa']
+    success_url=reverse_lazy('self_user_cases')
+    def test_func(self):
+        user_id = self.kwargs["pk"]
+        user = get_object_or_404(CustomUser, id=user_id)
+        return user == self.request.user
