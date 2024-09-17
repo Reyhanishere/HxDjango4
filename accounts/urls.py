28c9f78a4befdb509a07497adbd15bba1658e2ef
Ehsan Kalateh <96934557+EhsanKalateh@users.noreply.github.com>
2024-09-17 17:36:10 +0330

    add change_user_info url

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,4 +4,6 @@ from .views import SignUpView, self_user_cases
 urlpatterns = [
     path("signup/", SignUpView.as_view(), name="signup"),
     path("dashboard/", self_user_cases, name="self_user_cases"),
+    path("change/<int:pk>/", UserChangeInfoView.as_view(), name="change_user_info"),
+
 ]
