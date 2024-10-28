02f0a88aae1d884dd9e2814ad6a1d63ca33a23f1
Ehsan Kalateh <96934557+EhsanKalateh@users.noreply.github.com>
2024-10-28 03:55:02 +0330

    add AI_API_KEY

--- django_project/settings.py
+++ django_project/settings.py
@@ -145,4 +145,6 @@ EMAIL_HOST_USER = os.getenv("EMAIL_USER")
 EMAIL_HOST_PASSWORD=os.getenv("EMAIL_PASSWORD")
 EMAIL_USE_TLS = True
 DEFAULT_FROM_EMAIL = os.getenv("Reset_Password_Email")
+AI_API_KEY=os.getenv("AI_API_KEY")
+
 
