ac1516214e454e294d5ea903e237764ade5b26b8
Ehsan Kalateh <96934557+EhsanKalateh@users.noreply.github.com>
2024-10-28 05:48:21 +0330

    add AIReqResLog

--- cases/admin.py
+++ cases/admin.py
@@ -1,13 +1,7 @@
 from django.contrib import admin
 from .models import *
 
-
-class FollowUpInline(admin.TabularInline):
-    model = FollowUp
-    extra = 0
-
-
-class CommentInline(admin.TabularInline):  # new
+class CommentInline(admin.TabularInline):
     model = Comment
     extra = 0
 
@@ -18,9 +12,11 @@ class ReplyInline(admin.TabularInline):
 class CaseImageInline(admin.TabularInline):
     model = ImageCase
     extra = 1
+    
 class LabGraphInline(admin.TabularInline):
     model=LabGraphSelection
     extra=1
+    
 @admin.action(description="Deverify selected cases")
 def deverify_case(Case, request, queryset):
     queryset.update(verified=False)
@@ -53,8 +49,6 @@ class NotesAdmin(admin.ModelAdmin):
     list_display = ["title", "author", "verified", "delete", "date_created"]
     actions=[verify_note,deverify_note]
 
-
-
 admin.site.register(Choice)
 admin.site.register(Tag)
 admin.site.register(ImageCase)
@@ -63,9 +57,10 @@ admin.site.register(Rotation)
 admin.site.register(Case, CaseAdmin)
 admin.site.register(Picasso, PicassoAdmin)
 admin.site.register(Note,NotesAdmin)
-# admin.site.register(FollowUp)
 admin.site.register(Comment, CommentsAdmin)
 admin.site.register(Reply)
 admin.site.register(LabGraphSelection)
+admin.site.register(AIReqResLog)
+
 
 
