8fb1764e61e7996439c87ffc3223ad79f18e0928
Reyhanishere <159468603+Reyhanishere@users.noreply.github.com>
2024-11-07 00:04:22 -0800

    Added path for "premium" urls.py

--- pages/urls.py
+++ pages/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
-from .views import HomePageView, AboutPageView, TermsPageView, GraphsPageView
+from .views import HomePageView, AboutPageView, TermsPageView, GraphsPageView, PremiumPageView
 
 urlpatterns = [
     path("", HomePageView.as_view(), name="home"),
     path("about/", AboutPageView.as_view(), name="about"),
     path("terms/", TermsPageView.as_view(), name="terms"),
     path("cases/graphs/", GraphsPageView.as_view(), name="graphs"),
+    path("premium/", PremiumPageView.as_view(), name="premium"),
+
    
 ]
