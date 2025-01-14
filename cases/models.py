fa5569905c65f58b45325aabcc1d40c8b3dcdb7b
Ehsan Kalateh <96934557+EhsanKalateh@users.noreply.github.com>
2025-01-14 18:50:44 +0330

    import now to make slug

--- cases/models.py
+++ cases/models.py
@@ -1,6 +1,7 @@
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes.fields import GenericForeignKey
 
+from django.utils.timezone import now
 from django.conf import settings
 from django.db import models
 from django.urls import reverse
