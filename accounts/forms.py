543f8012abf4b43a9d3efc83fae29d1567d3178a
Ehsan Kalateh <96934557+EhsanKalateh@users.noreply.github.com>
2025-02-23 15:45:15 +0330

    add en_name to form fields and define clean function for name

--- accounts/forms.py
+++ accounts/forms.py
@@ -12,15 +12,15 @@ class CustomUserCreationForm(UserCreationForm):
 class CustomUserChangeForm(UserChangeForm):
     class Meta:
         model = CustomUser
-        fields=['first_name', 'last_name', 'degree', 'university','fn_fa','ln_fa',]
+        fields=['first_name', 'last_name', 'degree', 'university','fn_fa','ln_fa','en_name']
+        
     first_name=forms.CharField(label='نام به انگلیسی')
     last_name=forms.CharField(label='نام خانوادگی به انگلیسی')
-    # degree=forms.ChoiceField(label='ردۀ تحصیلی')
 
-    # en_name = forms.BooleanField(
-    #     label='نام من را به انگلیسی نشان بده.',
-    #     required=False
-    # )
+    en_name = forms.BooleanField(
+        label='نام من را به انگلیسی نشان بده.',
+        required=False
+    )
     fn_fa = forms.CharField(
         label='نام به فارسی',
         required=False
@@ -29,15 +29,15 @@ class CustomUserChangeForm(UserChangeForm):
         label='نام خانوادگی به فارسی',
         required=False
     )
+    password=forms.HiddenInput()
 
-    # def clean(self):
-    #     cleaned_data = super().clean()
-    #     en_name = cleaned_data.get('en_name')
-    #     fn_fa = cleaned_data.get('fn_fa')
-    #     ln_fa = cleaned_data.get('ln_fa')
+    def clean(self):
+        cleaned_data = super().clean()
+        en_name = cleaned_data.get('en_name')
+        fn_fa = cleaned_data.get('fn_fa')
+        ln_fa = cleaned_data.get('ln_fa')
 
-    #     if en_name is False and (not fn_fa or not ln_fa):
-    #         raise forms.ValidationError("برای اینکه نام فارسی شما را نمایش دهیم، باید نام خود را به فارسی وارد کنید.")
+        if en_name is False and (not fn_fa or not ln_fa):
+            raise forms.ValidationError("برای اینکه نام فارسی شما را نمایش دهیم، باید نام خود را به فارسی وارد کنید.")
 
-    #     return cleaned_data
-    
+        return cleaned_data
