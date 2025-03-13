e396036173bb2f948fbb502b0d3a4dd7094f21de
Ehsan Kalateh <96934557+EhsanKalateh@users.noreply.github.com>
2025-03-13 19:32:39 +0330

    Add load_z_score_table function

--- cases/utils.py
+++ cases/utils.py
@@ -7,3 +7,14 @@ def load_pedi_weight_data():
         return json.load(file)
 
 weight_data = load_pedi_weight_data()
+
+#### ------------------ ####
+
+def load_z_score_table():
+    json_file_path = os.path.join(os.path.dirname(__file__), '../static/data/z_table_abstract.json')
+    with open(json_file_path, 'r') as file:
+        return json.load(file)
+
+z_score_table_data = load_z_score_table()
+
+#### ------------------ ####
