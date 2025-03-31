ad7a0e1b6a49430657edda00841f35f042a6d2d4
Ehsan Kalateh <96934557+EhsanKalateh@users.noreply.github.com>
2025-04-01 01:19:39 +0330

    add my favorite, CalCate, CaList

--- calculus/urls.py
+++ calculus/urls.py
@@ -4,6 +4,9 @@ from .views import *
 
 
 urlpatterns = [
+    path('', CalculusListView.as_view(), name='calculus_main'),
+    path('calcate/<slug:link>/', CalcCateDetailView.as_view(), name='calcates_detail'),
+
     path('calculi/pedi_w_zscore/', CalculateWeightZScoreView.as_view(), name='cal_wzs'),
     path('calculi/pedi_l_zscore/', CalculateLengthZScoreView.as_view(), name='cal_lzs'),
     path('calculi/pedi_hc_zscore/', CalculateHCZScoreView.as_view(), name='cal_hczs'),
@@ -12,5 +15,6 @@ urlpatterns = [
     path('calculi/<slug:link>_page/', calculi_page_view, name='calculi_page'),
 
     path('calculi/like/', like_calculi, name='calculi_like'),
-    
+
+    path('my_favorites/', favorite_calculus, name='favorite_calculus'),
 ]
