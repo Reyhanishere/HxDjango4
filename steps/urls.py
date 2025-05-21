9d9c400978894245d81b7e68e9afd0d16542f2e4
Ehsan Kalateh <96934557+EhsanKalateh@users.noreply.github.com>
2025-05-21 06:16:47 +0330

    Add submit race score, ranking page, race page

--- steps/urls.py
+++ steps/urls.py
@@ -3,6 +3,9 @@ from .views import *
 
 urlpatterns = [
     path('', StepListView.as_view(), name='step_list'),
+    path('race/<int:pk>/', StepRaceDetailView.as_view(), name='step_race_detail'),
     path('step/<slug:slug>/', StepDetailView.as_view(), name='step_detail'),
+    path('submit_race_score/<int:race_id>/', submit_race_score, name='submit_race_score'),
+    path('race/<int:race_id>/ranking/', ranking_page, name='ranking_page'),
     path('blocks/<int:block_id>/submit/', submit_answer, name='submit_answer'),
 ]
