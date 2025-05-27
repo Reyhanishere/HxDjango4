eeb29351a120bf0e21eb73368f602ff96c786d6c
Ehsan Kalateh <96934557+EhsanKalateh@users.noreply.github.com>
2025-05-27 05:45:04 +0330

    New imports, calculate age, print view, changes in record view, login required, doctor set as user

--- doctors/views.py
+++ doctors/views.py
@@ -1,11 +1,17 @@
+import requests
+from decimal import Decimal, ROUND_HALF_UP
+from datetime import date, timedelta
+import json
+
 from django.shortcuts import render, redirect, get_object_or_404
+from django.utils.timezone import localtime
+from django.contrib.auth.decorators import login_required
 from django.urls import reverse_lazy
 from django.views.generic import CreateView, UpdateView
 
-from .models import Patient
-from .forms import IDCheckForm, NewPatientForm
-
+from .models import *
 from .forms import *
+from accounts.models import *
 
 def check_or_create_patient(request):
     if request.method == 'POST':
@@ -40,20 +46,42 @@ def create_patient(request):
     return render(request, 'doctors/new_patient.html', {'form': form, 'personal_id': personal_id})
 
 
-import requests
-from decimal import Decimal, ROUND_HALF_UP
-from collections import defaultdict
-from datetime import date
-import json
 
-from django.shortcuts import render, get_object_or_404
-from django.utils.timezone import localtime
-from django.contrib.auth.decorators import login_required
+def calculate_age_extended(birth_date):
+    today = date.today()
+    
+    years = today.year - birth_date.year
+    months = today.month - birth_date.month
+    days = today.day - birth_date.day
+    
+    # Adjust for negative months/days
+    if days < 0:
+        months -= 1
+        # Get last day of previous month
+        last_month = today.replace(day=1) - timedelta(days=1)
+        days += last_month.day
+    
+    if months < 0:
+        years -= 1
+        months += 12
 
-from .models import *
-from accounts.models import *
+    if years==0:
+        year_text=''
+    else:
+        year_text='{} سال و '.format(years)
 
-# @login_required
+    if months==0:
+        month_text=''
+    else:
+        month_text='{} ماه و '.format(months)
+    if days==0:
+        day_text=''
+    else:
+        day_text='{} روز'.format(days)
+    return year_text + month_text + day_text
+
+
+@login_required
 def calculate_zscore(request, personal_id):
     patient = get_object_or_404(Patient, personal_id=personal_id)
     previous = Record.objects.filter(patient=patient).order_by('-record_add_date').first()
@@ -67,8 +95,6 @@ def calculate_zscore(request, personal_id):
         today = date.today()
         days = (today - patient.birth_date).days
         age_months = Decimal(days / 30.4375).quantize(Decimal('0.5'), rounding=ROUND_HALF_UP)
-        age_months=round(float(age_months),0)
-
         
         if patient.gender =='پسر':
             gender = '1'
@@ -77,7 +103,7 @@ def calculate_zscore(request, personal_id):
         # Send request to the external API
         try:
             response = requests.get(
-                "https://mdpt.ir/calculus/calculi/pedi_all_zscores/",
+                "https://medepartout.ir/calculus/calculi/pedi_all_zscores/",
                 params={
                     'gender': gender,
                     'age_months': age_months,
@@ -85,7 +111,7 @@ def calculate_zscore(request, personal_id):
                     'height': height,
                 }
             )
-            doctor = get_object_or_404(Doctor, user__username='Ehsan')
+            doctor = get_object_or_404(Doctor, user=request.user)
             response.raise_for_status()
             result = response.json()
             Record.objects.create(
@@ -97,13 +123,13 @@ def calculate_zscore(request, personal_id):
                 height=height,
                 # hc=hc,
                 # Extract values from the response
-                weight_z=result['weight']['z_score'],
-                weight_p=result['weight']['percentile'],
-                height_z=result['height']['z_score'],
-                height_p=result['height']['percentile'],
-                bmi=result['bmi']['value'],
-                bmi_z=result['bmi']['z_score'],
-                bmi_p=result['bmi']['percentile']
+                weight_z=result["weight"]["z_score"],
+                weight_p=result["weight"]["percentile"],
+                height_z=result["height"]["z_score"],
+                height_p=result["height"]["percentile"],
+                bmi=result["bmi"]["value"],
+                bmi_z=result["bmi"]["z_score"],
+                bmi_p=result["bmi"]["percentile"]
             )
         
         except requests.RequestException as e:
@@ -117,34 +143,30 @@ def calculate_zscore(request, personal_id):
 
 
 
+from .templatetags.tags import *
 
-# You said you have a Jalali converter, so we assume: to_jalali(date)
-from .templatetags.tags import *  # hypothetical utility function
-
-# @login_required
+@login_required
 def patient_record_view(request, personal_id):
-    doctor = get_object_or_404(Doctor, user__username='Ehsan')
+    doctor = get_object_or_404(Doctor, user=request.user)
     patient = get_object_or_404(Patient, personal_id=personal_id)
-
+    patient_age = calculate_age_extended(patient.birth_date)
     # Filter records for this doctor and this patient
     records = Record.objects.filter(doctor=doctor, patient=patient).order_by('-record_add_date')
     records_reverse=Record.objects.filter(doctor=doctor, patient=patient).order_by('record_add_date')
 
-    # Convert all records' dates to Jalali
-    # all_dates_jalali = [to_jalali(str(record.record_add_date.strftime("%Y %m %d"))) for record in records]
 
     # Get all Z-scores
     all_zscores = {
         'weight_z': [record.weight_z for record in records_reverse],
         'height_z': [record.height_z for record in records_reverse],
         'bmi_z': [record.bmi_z for record in records_reverse],
-        'labels': [to_jalali(str(record.record_add_date.strftime("%Y %m %d"))) for record in records_reverse]
+        'labels': [j_date(record.record_add_date, 'digit, long') for record in records_reverse]
     }
 
     # Last 3 records (latest to earliest)
-    recent_records = records[:3]
+    recent_records = records[:5]
     recent_data = [{
-        'date': to_jalali(r.record_add_date.strftime("%Y %m %d")),
+        'date': j_date(r.record_add_date, 'name, long'),
         'weight': r.weight,
         'height': r.height,
         'bmi': round(r.bmi,2),
@@ -158,16 +180,17 @@ def patient_record_view(request, personal_id):
 
     # Last 5 visits (date + one or more value fields)
     last_visits = {
-        'weight': [record.weight for record in records_reverse[:5]],
-        'height': [record.height for record in records_reverse[:5]],
-        'bmi': [record.bmi for record in records_reverse[:5]],
-        'labels': [to_jalali(str(record.record_add_date.strftime("%Y %m %d"))) for record in records_reverse[:5]]
+        'weight': [record.weight for record in records_reverse[:10]],
+        'height': [record.height for record in records_reverse[:10]],
+        'bmi': [record.bmi for record in records_reverse[:10]],
+        'labels': [j_date(record.record_add_date, 'digit, long') for record in records_reverse[:10]]
     }
     
 
     context = {
         'doctor': doctor,
         'patient': patient,
+        'patient_age': patient_age,
         # 'all_zscores': all_zscores,
         # 'all_dates': all_dates_jalali,
         'recent_records': recent_data,
@@ -177,3 +200,59 @@ def patient_record_view(request, personal_id):
     context['last_visits'] = json.dumps(last_visits)
 
     return render(request, 'doctors/patient_records.html', context)
+
+@login_required
+def patient_record_print_view(request, personal_id):
+    doctor = get_object_or_404(Doctor, user=request.user)
+    patient = get_object_or_404(Patient, personal_id=personal_id)
+    patient_age = calculate_age_extended(patient.birth_date)
+    # Filter records for this doctor and this patient
+    records = Record.objects.filter(doctor=doctor, patient=patient).order_by('-record_add_date')
+    records_reverse=Record.objects.filter(doctor=doctor, patient=patient).order_by('record_add_date')
+
+
+    # Get all Z-scores
+    all_zscores = {
+        'weight_z': [record.weight_z for record in records_reverse],
+        'height_z': [record.height_z for record in records_reverse],
+        'bmi_z': [record.bmi_z for record in records_reverse],
+        'labels': [j_date(record.record_add_date, 'digit, long') for record in records_reverse]
+    }
+
+    # Last 3 records (latest to earliest)
+    recent_records = records[:5]
+    recent_data = [{
+        'date': j_date(r.record_add_date, 'name, long'),
+        'weight': r.weight,
+        'height': r.height,
+        'bmi': round(r.bmi,2),
+        'weight_z': r.weight_z,
+        'height_z': r.height_z,
+        'bmi_z': r.bmi_z,
+        'weight_p': r.weight_p,
+        'height_p': r.height_p,
+        'bmi_p': r.bmi_p
+    } for r in recent_records]
+
+    # Last 5 visits (date + one or more value fields)
+    last_visits = {
+        'weight': [record.weight for record in records_reverse[:10]],
+        'height': [record.height for record in records_reverse[:10]],
+        'bmi': [record.bmi for record in records_reverse[:10]],
+        'labels': [j_date(record.record_add_date, 'digit, long') for record in records_reverse[:10]]
+    }
+    
+
+    context = {
+        'doctor': doctor,
+        'patient': patient,
+        'patient_age': patient_age,
+        # 'all_zscores': all_zscores,
+        # 'all_dates': all_dates_jalali,
+        'recent_records': recent_data,
+        # 'last_visits': last_visits,
+    }
+    context['all_zscores'] = json.dumps(all_zscores)
+    context['last_visits'] = json.dumps(last_visits)
+
+    return render(request, 'doctors/patient_records_print.html', context)
