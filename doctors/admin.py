901d8f1515a7f492a3a7ab567131a19dca43d4f8
Ehsan Kalateh <96934557+EhsanKalateh@users.noreply.github.com>
2025-06-08 00:10:27 +0330

    Add Recommendation, Inlines of Patient

--- doctors/admin.py
+++ doctors/admin.py
@@ -1,8 +1,20 @@
 from django.contrib import admin
 from .models import *
 
+class RecordInline(admin.TabularInline):
+    model = Record
+    extra= 1
+
+class RecomInline(admin.TabularInline):
+    model = Recommendation
+    extra= 1
+
+class PatientAdmin(admin.ModelAdmin):
+    inlines = [RecomInline, RecordInline]
+
 # Register your models here.
 admin.site.register(Record)
+admin.site.register(Recommendation)
 admin.site.register(Doctor)
-admin.site.register(Patient)
+admin.site.register(Patient, PatientAdmin)
 admin.site.register(Company)
