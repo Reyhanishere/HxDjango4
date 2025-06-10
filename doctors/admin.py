fcbe2f76977a3c6682edc33fe899e034fa583eee
Ehsan Kalateh <96934557+EhsanKalateh@users.noreply.github.com>
2025-06-10 11:43:15 +0330

    list_display for patient and record

--- doctors/admin.py
+++ doctors/admin.py
@@ -11,9 +11,16 @@ class RecomInline(admin.TabularInline):
 
 class PatientAdmin(admin.ModelAdmin):
     inlines = [RecomInline, RecordInline]
+    list_display  = ('name','birth_date','gender','personal_id','created_at')
+
+    
+class RecordAdmin(admin.ModelAdmin):
+    list_display  = ('patient','doctor', 'record_add_date',)
+    
+
 
 # Register your models here.
-admin.site.register(Record)
+admin.site.register(Record, RecordAdmin)
 admin.site.register(Recommendation)
 admin.site.register(Doctor)
 admin.site.register(Patient, PatientAdmin)
