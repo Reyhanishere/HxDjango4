a02c529edaaaeded6023622b5669fafd2097d615
Ehsan Kalateh <96934557+EhsanKalateh@users.noreply.github.com>
2025-06-24 06:08:09 +0330

    Add temp URL for Chart

--- doctors/urls.py
+++ doctors/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('patient/<slug:personal_id>/overview/', patient_record_view, name='patient_records'),
     path('patient/<slug:personal_id>/overview/print/', patient_record_print_view, name='patient_records_print'),
     path('patients_list/', patients_list_view, name='patients_list'),
-    path('patient/<slug:personal_id>/edit/', patient_update_view, name='patient_update')
+    path('patient/<slug:personal_id>/edit/', patient_update_view, name='patient_update'),
+    path('chart/', chart),
 ]
