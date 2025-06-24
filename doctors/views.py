8527b0b1f6f99472bef6af07822c44a1d9317aac
Ehsan Kalateh <96934557+EhsanKalateh@users.noreply.github.com>
2025-06-24 05:56:01 +0330

    Add Chart view for Ps, Set Defaults for New Patient, Customize data no. of tables and charts, Externalize set_is_wl and set_recent_data

--- doctors/views.py
+++ doctors/views.py
@@ -37,58 +37,18 @@ def create_patient(request):
         return redirect('corc_patient')
 
     if request.method == 'POST':
-        form = NewPatientForm(request.POST)
+        form = NewPatientForm(request.POST, is_default_girl=doctor.is_girl, is_date_reverse=doctor.is_date_reverse)
         if form.is_valid():
             patient = form.save(commit=False)
             patient.personal_id = personal_id
+            patient.created_by = doctor
             patient.save()
             del request.session['pending_personal_id']
             return redirect('zscore', personal_id=patient.personal_id)
     else:
-        form = NewPatientForm()
+        form = NewPatientForm(is_default_girl=doctor.is_girl, is_date_reverse=doctor.is_date_reverse)
     return render(request, 'doctors/new_patient.html', {'doctor':doctor,'form': form, 'personal_id': personal_id})
 
-
-
-def calculate_age_extended(birth_date):
-    today = date.today()
-    
-    years = today.year - birth_date.year
-    months = today.month - birth_date.month
-    days = today.day - birth_date.day
-    
-    # Adjust for negative months/days
-    if days < 0:
-        months -= 1
-        # Get last day of previous month
-        last_month = today.replace(day=1) - timedelta(days=1)
-        days += last_month.day
-    
-    if months < 0:
-        years -= 1
-        months += 12
-
-    if years==0:
-        year_text=''
-    else:
-        year_text='{} سال و '.format(years)
-
-    if months==0:
-        month_text=''
-    else:
-        month_text='{} ماه و '.format(months)
-    if days==0:
-        day_text=''
-    else:
-        day_text='{} روز'.format(days)
-    return year_text + month_text + day_text
-
-def to_five(value):
-    value *= 10
-    value=round(value)
-    while value % 5 != 0:
-        value += 1
-    return value / 10
 
 @login_required
 def patient_record_view(request, personal_id):
@@ -97,59 +57,50 @@ def patient_record_view(request, personal_id):
     patient_age = calculate_age_extended(patient.birth_date)
     # Filter records for this doctor and this patient
     records = Record.objects.filter(doctor=doctor, patient=patient).order_by('-record_date')
-    records_reverse=Record.objects.filter(doctor=doctor, patient=patient).order_by('record_date')
-
+    
+    # Set is_wl
+    is_wl = set_is_wl(records.first())
 
     # Get all Z-scores
+    z_scores_list = list(records[:doctor.z_score_count])
+    z_scores_list.reverse()
     all_zscores = {
-        'weight_z': [record.weight_z for record in records_reverse],
-        'height_z': [record.height_z for record in records_reverse],
-        'bmi_z': [record.bmi_z for record in records_reverse],
-        'hc_z': [record.hc_z for record in records_reverse] if patient.get_age_months() <= 36 else [],
+        'weight_z': [record.weight_z for record in z_scores_list],
+        'height_z': [record.height_z for record in z_scores_list],
+        'bmi_z': [record.bmi_z for record in z_scores_list],
+        'hc_z': [record.hc_z for record in z_scores_list] if patient.get_age_months() <= 36 else [],
+        'wl_z': [record.wl_z for record in z_scores_list] if is_wl else [],
 
-        'labels': [j_date(record.record_date, 'digit, long') for record in records_reverse]
+        'labels': [j_date(record.record_date, 'digit, long') for record in z_scores_list]
     }
 
-    # Last 5 records (latest to earliest)
-    recent_records = records[:5]
-    recent_data = [{
-        'date': j_date(r.record_date, 'name, long'),
-        'weight': r.weight,
-        'height': r.height,
-        'bmi': round(r.bmi,2),
-        'hc': r.hc,
-        'weight_z': r.weight_z,
-        'height_z': r.height_z,
-        'bmi_z': r.bmi_z,
-        'hc_z': r.hc_z,
-        'weight_p': r.weight_p,
-        'height_p': r.height_p,
-        'bmi_p': r.bmi_p,
-        'hc_p': r.hc_p,
-    } for r in recent_records]
+    # Last 5 records (latest to earliest) for Table
+    recent_records = records[:doctor.table_rows_count]
+    recent_data = set_recent_data(records=recent_records, age=patient.get_age_months(), is_wl=is_wl)
 
-    # Last 10 visits (date + one or more value fields)
-    last_records = list(records[:10])
+    # Last n visits for mini charts
+    last_records = list(records[:doctor.mini_charts_data_count])
     last_records.reverse()
     last_visits = {
         'weight': [record.weight for record in last_records],
         'height': [record.height for record in last_records],
-        'bmi': [record.bmi for record in last_records],
         'hc': [record.hc for record in last_records],
+        'wl_p50': [record.wl_p50 for record in last_records],
+
         'labels': [j_date(record.record_date, 'digit, long') for record in last_records]
     }
+    if patient.get_age_months() >= 24:
+        last_visits['bmi'] = [record.bmi for record in last_records]
     
     recoms = Recommendation.objects.filter(doctor=doctor, patient=patient).order_by('-add_date')[:5]
-
+    
     context = {
         'doctor': doctor,
         'patient': patient,
         'patient_age': patient_age,
-        # 'all_zscores': all_zscores,
-        # 'all_dates': all_dates_jalali,
         'recent_records': recent_data,
-        # 'last_visits': last_visits,
         'recoms': recoms,
+        'is_wl': is_wl,
     }
     context['all_zscores'] = json.dumps(all_zscores)
     context['last_visits'] = json.dumps(last_visits)
@@ -167,62 +118,56 @@ def patient_record_print_view(request, personal_id):
     age_months = min(age_months, 240)
     # Filter records for this doctor and this patient
     records = Record.objects.filter(doctor=doctor, patient=patient).order_by('-record_date')
-    records_reverse=Record.objects.filter(doctor=doctor, patient=patient).order_by('record_date')
 
+    # Set recommendations
     last_record = records.first()
     if last_record:
         considerations, recommendations = make_recom_and_considers(age_months=age_months, last_record=last_record)
     else: 
         considerations, recommendations = {}, {}
+    
+    # Set is_wl
+    is_wl = set_is_wl(records.first())
+    print(is_wl)
 
     # Get all Z-scores
+    z_scores_list = list(records[:doctor.z_score_count])
+    z_scores_list.reverse()
     all_zscores = {
-        'weight_z': [record.weight_z for record in records_reverse],
-        'height_z': [record.height_z for record in records_reverse],
-        'bmi_z': [record.bmi_z for record in records_reverse],
-        'hc_z': [record.hc_z for record in records_reverse] if patient.get_age_months() <= 36 else [],
+        'weight_z': [record.weight_z for record in z_scores_list],
+        'height_z': [record.height_z for record in z_scores_list],
+        'bmi_z': [record.bmi_z for record in z_scores_list],
+        'hc_z': [record.hc_z for record in z_scores_list] if patient.get_age_months() <= 36 else [],
+        'wl_z': [record.wl_z for record in z_scores_list] if is_wl else [],
 
-        'labels': [j_date(record.record_date, 'digit, long') for record in records_reverse]
+        'labels': [j_date(record.record_date, 'digit, long') for record in z_scores_list]
     }
 
-    # Last 5 records (latest to earliest)
+    # Last n records (latest to earliest) for Table
     recent_records = records[:5]
-    recent_data = [{
-        'date': j_date(r.record_date, 'name, long'),
-        'weight': r.weight,
-        'height': r.height,
-        'bmi': round(r.bmi,2),
-        'hc': r.hc,
-        'weight_z': r.weight_z,
-        'height_z': r.height_z,
-        'bmi_z': r.bmi_z,
-        'hc_z': r.hc_z,
-        'weight_p': r.weight_p,
-        'height_p': r.height_p,
-        'bmi_p': r.bmi_p,
-        'hc_p': r.hc_p,
-    } for r in recent_records]
+    recent_data = set_recent_data(records=recent_records, age=patient.get_age_months(), is_wl=is_wl)
 
-    # Last 10 visits (date + one or more value fields)
-    last_records = list(records[:10])
+    # Last n visits for mini charts
+    last_records = list(records[:doctor.mini_charts_data_count])
     last_records.reverse()
     last_visits = {
         'weight': [record.weight for record in last_records],
         'height': [record.height for record in last_records],
-        'bmi': [record.bmi for record in last_records],
         'hc': [record.hc for record in last_records],
-        'labels': [j_date(record.record_date, 'digit, long') for record in records_reverse[:10]]
+        'wl_p50': [record.wl_p50 for record in last_records],
+
+        'labels': [j_date(record.record_date, 'digit, short') for record in last_records]
     }
+    if patient.get_age_months() >= 24:
+        last_visits['bmi'] = [record.bmi for record in last_records]
     
     recoms = Recommendation.objects.filter(doctor=doctor, patient=patient)
     last_recom = recoms.last()
     today_recom = Recommendation.objects.filter(doctor=doctor, patient=patient, add_date=date.today()).last()
     
     if request.method == 'POST':
-        # Get the recommendation text from the form
         recommendation_text = request.POST.get('recommendation_text')
-        if recommendation_text:  # Make sure there's text
-
+        if recommendation_text:
             if today_recom:
                 today_recom.text = recommendation_text
                 today_recom.save()
@@ -231,7 +176,6 @@ def patient_record_print_view(request, personal_id):
                     doctor=doctor,
                     patient=patient,
                     text=recommendation_text,
-                    # Add any other required fields here
                 )
         else:
             pass
@@ -242,14 +186,12 @@ def patient_record_print_view(request, personal_id):
         'doctor': doctor,
         'patient': patient,
         'patient_age': patient_age,
-        # 'all_zscores': all_zscores,
-        # 'all_dates': all_dates_jalali,
         'recent_records': recent_data,
         'last_recom':last_recom.text if last_recom else '',
         'considerations' : considerations,
         'recommendations' : recommendations,
+        'is_wl': is_wl,
 
-        # 'last_visits': last_visits,
     }
     context['all_zscores'] = json.dumps(all_zscores)
     context['last_visits'] = json.dumps(last_visits)
@@ -353,7 +295,7 @@ def calculate_zscore(request, personal_id):
             if existing_qs.exists() and not request.POST.get('force') and not request.POST.get('update'):
                 return JsonResponse({
                     'status': 'exists',
-                    'message': f"A record already exists for {record_date}.",
+                    'message': "در این تاریخ یک رکورد ثبت شده است.",
                 })
 
             try:
@@ -367,21 +309,29 @@ def calculate_zscore(request, personal_id):
                     record.hc = hc
                     
                     record.age_months = age_months
-                    record.weight_z = result_content["weight"]["z_score"]
-                    record.weight_p = result_content["weight"]["percentile"]
+                    record.weight_z = result_content["Weight"]["z_score"]
+                    record.weight_p = result_content["Weight"]["percentile"]
 
-                    record.height_z = result_content["height"]["z_score"]
-                    record.height_p = result_content["height"]["percentile"]
+                    record.height_z = result_content["Height"]["z_score"]
+                    record.height_p = result_content["Height"]["percentile"]
+                    
+                    if "Head Circumference" in result_content.keys():
+                        record.hc_z = result_content["Head Circumference"]["z_score"]
+                        record.hc_p = result_content["Head Circumference"]["percentile"]
+                    
+                    if "Weight for Length" in result_content.keys():
+                        record.wl_p50 = result_content["Weight for Length"]["value"]
+                        record.wl_z = result_content["Weight for Length"]["z_score"]
+                        record.wl_p = result_content["Weight for Length"]["percentile"]
 
-                    record.hc_z = result_content["hc"]["z_score"]
-                    record.hc_p = result_content["hc"]["percentile"]
+                    if result_content["BMI"]:
+                        record.bmi = result_content["BMI"]["value"]
+                        record.bmi_z = result_content["BMI"]["z_score"]
+                        record.bmi_p = result_content["BMI"]["percentile"]
 
-                    record.bmi = result_content["bmi"]["value"]
-                    record.bmi_z = result_content["bmi"]["z_score"]
-                    record.bmi_p = result_content["bmi"]["percentile"]
                     record.save()
                     return JsonResponse({'status': 'updated', 'result': result_content})
-
+                
                 # Create new record
                 Record.objects.create(
                     doctor=doctor,
@@ -392,21 +342,27 @@ def calculate_zscore(request, personal_id):
                     weight=weight,
                     height=height,
                     hc=hc,
-                    weight_z=result_content["weight"]["z_score"],
-                    weight_p=result_content["weight"]["percentile"],
-                    height_z=result_content["height"]["z_score"],
-                    height_p=result_content["height"]["percentile"],
-                    hc_z = result_content["hc"]["z_score"],
-                    hc_p = result_content["hc"]["percentile"],
-                    bmi=result_content["bmi"]["value"],
-                    bmi_z=result_content["bmi"]["z_score"],
-                    bmi_p=result_content["bmi"]["percentile"]
+                    weight_z=result_content["Weight"]["z_score"],
+                    weight_p=result_content["Weight"]["percentile"],
+                    height_z=result_content["Height"]["z_score"],
+                    height_p=result_content["Height"]["percentile"],
+                    hc_z = result_content["Head Circumference"]["z_score"] if "Head Circumference" in result_content.keys() else 0,
+                    hc_p = result_content["Head Circumference"]["percentile"] if "Head Circumference" in result_content.keys() else 50,
+                    wl_p50 = result_content["Weight for Length"]["value"] if "Weight for Length" in result_content.keys() else None,
+                    wl_z = result_content["Weight for Length"]["z_score"] if "Weight for Length" in result_content.keys() else None,
+                    wl_p = result_content["Weight for Length"]["percentile"] if "Weight for Length" in result_content.keys() else None,
+                    bmi=result_content["BMI"]["value"],
+                    bmi_z=result_content["BMI"]["z_score"],
+                    bmi_p=result_content["BMI"]["percentile"]
                 )
                 return JsonResponse({'status': 'created', 'result': result_content})
 
             except Exception as e:
                 return JsonResponse({'status': 'error', 'error': str(e)})
+        else:
+            errs = list(form.errors.values())
 
+            return JsonResponse({'status': 'error', 'error': errs})
     else:
         initial_data = {}
         if previous:
@@ -424,3 +380,75 @@ def calculate_zscore(request, personal_id):
         'result': result,
         'previous': previous,
     })
+
+import decimal
+def drange(x, y, jump):
+  while x < y:
+    yield float(x)
+    x += jump
+
+def chart(request):
+    patient = get_object_or_404(Patient, personal_id='0670893404')
+    records = Record.objects.filter(patient=patient).order_by('-record_date')[:10]
+    first_age = records[9].age_months
+    if first_age%1 != 0.5:
+        first_age -= 0.5
+    last_age = Record.objects.filter(patient=patient).last().age_months
+    if last_age%1 != 0.5:
+        last_age += 0.5
+    age_list = []
+    
+    # for i in list(drange(first_age-3, last_age+7, 10)):
+
+    for i in range(int(10*(first_age-3)), int(10*(last_age+7)), 10):
+        age_list.append(i/10)
+    
+    first_ind = age_list.index(first_age) - 3
+    last_ind = age_list.index(last_age) + 7
+    age_list = age_list[first_ind:last_ind]
+    kid_data = []
+    for i in age_list:
+        for r in records:
+            if r.age_months == i or r.age_months-0.5 == i:
+                kid_data.append(r.weight)
+                break        
+        else:
+            kid_data.append(10)
+    # age = 50.5
+    
+    gender = '1'
+    agemos = wps_data[gender]['agemos']
+    # ind = agemos.index(age)
+    agemos = agemos[first_ind:last_ind]
+    p3  = wps_data[gender]['P3'][first_ind:last_ind]
+    p5  = wps_data[gender]['P5'][first_ind:last_ind]
+    p10 = wps_data[gender]['P10'][first_ind:last_ind]
+    p25 = wps_data[gender]['P25'][first_ind:last_ind]
+    p50 = wps_data[gender]['P50'][first_ind:last_ind]
+    p75 = wps_data[gender]['P75'][first_ind:last_ind]
+    p90 = wps_data[gender]['P90'][first_ind:last_ind]
+    p95 = wps_data[gender]['P95'][first_ind:last_ind]
+    p97 = wps_data[gender]['P97'][first_ind:last_ind]
+    # print(len(kid_data))
+    # print(len(p3))
+    # print(len(agemos))
+    # print(len(p97))
+
+
+    x = [24, 24.5, 25.5, 26.5, 27.5, 28.5, 29.5, 30.5, 31.5, 32.5, 33.5, 34.5, 35.5]
+    result = {
+        'kid_data':kid_data,
+        'agemos' : agemos,
+        'p3': p3,
+        'p5': p5,
+        'p10': p10,
+        'p25': p25,
+        'p50': p50,
+        'p75': p75,
+        'p90': p90,
+        'p95': p95,
+        'p97': p97,
+    }
+    p3 = [i for i in list(wps_data[gender].values())[0]]
+    wps_data[gender]
+    return render(request, 'doctors/chart.html', result)
