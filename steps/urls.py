e562765a64710c3dfcef1a90de6b21c9431a4282
Ehsan Kalateh <96934557+EhsanKalateh@users.noreply.github.com>
2025-10-20 19:28:56 +0330

    Add Course

--- steps/urls.py
+++ steps/urls.py
@@ -9,10 +9,19 @@ urlpatterns = [
     path('interactive_step/<int:step_id>/blocks/<int:block_number>/', load_interactive_block, name='get_block'),
     path('interactive_step/<slug:slug>/', InteractiveStepDetailView.as_view(), name='interactive_step_detail'),
     path('interactive_step/<slug:slug>/graph_code/', InteractiveStepGraphVizz.as_view(), name='interactive_step_graph_code'),
+
     
     path('submit_race_score/<int:race_id>/', submit_race_score, name='submit_race_score'),
+
     path('race/<int:race_id>/ranking/', ranking_page, name='ranking_page'),
+
     path('blocks/<int:block_id>/submit/', submit_answer, name='submit_answer'),
+
+    path('course/<uuid:uuid>/', course_detail, name='course_detail'),
+    path('course/<uuid:uuid>/register/', register_course, name='register_course'),
+    path('course/<uuid:uuid>/race/<int:race_id>/', StepCourseRaceDetailView.as_view(), name='course_race_view'),
+    path('course/<uuid:uuid>/race/<int:race_id>/submit_race_score/', submit_course_race_score, name='submit_course_race_score'),
 ]
+
 
 
